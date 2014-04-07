3128d9b2d8ece0906e91c20227eb4dcdba232788
Richard Hu <linuxfae@technexion.com>
2014-04-07 15:57:04 +0300

    spl: mx6: add an spl.h for mx6
    Create arch/arm/include/asm/arch-mx6/spl.h so that
    SPL support could be compiled.
    
    Signed-off-by: Nikita Kiryanov <nikita@compulab.co.il>

---
+++ arch/arm/include/asm/arch-mx6/spl.h
@@ -0,0 +1,21 @@
+/*
+* Copyright (C) 2013 TechNexion Ltd.
+*
+* Author: Richard Hu <linuxfae@technexion.com>
+*
+* SPDX-License-Identifier:	GPL-2.0+
+*/
+
+#ifndef __ASM_ARCH_SPL_H__
+#define __ASM_ARCH_SPL_H__
+
+#define BOOT_DEVICE_MMC1	0
+#define BOOT_DEVICE_MMC2	1
+#define BOOT_DEVICE_MMC2_2	2
+#define BOOT_DEVICE_NAND	3
+#define BOOT_DEVICE_SATA	4
+#define BOOT_DEVICE_USBETH	5
+#define BOOT_DEVICE_SPI		6
+#define BOOT_DEVICE_NONE	7
+
+#endif	/* __ASM_ARCH_SPL_H__ */
