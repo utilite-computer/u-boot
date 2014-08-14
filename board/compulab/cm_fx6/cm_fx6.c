c3fc8329861c2721ffd67886fe23ec2fd1bdcca9
Nikita Kiryanov <nikita@compulab.co.il>
2014-08-14 10:24:27 +0300

    arm: mx6: cm_fx6: pass 2nd nic mac addr to Linux
    Obtain 2nd NIC MAC address from baseboard EEPROM and pass it to Linux.
    
    Signed-off-by: Nikita Kiryanov <nikita@compulab.co.il>

--- board/compulab/cm_fx6/cm_fx6.c
+++ board/compulab/cm_fx6/cm_fx6.c
@@ -338,32 +338,33 @@ static void cm_fx6_set_enet_iomux(void)
 		       MUX_PAD_CTRL(ENET_PAD_CTRL));
 }
 
-static int handle_mac_address(void)
+static int handle_mac_address(char *env_var, uint eeprom_bus)
 {
 	unsigned char enetaddr[6];
 	int rc;
 
-	rc = eth_getenv_enetaddr("ethaddr", enetaddr);
+	rc = eth_getenv_enetaddr(env_var, enetaddr);
 	if (rc)
 		return 0;
 
-	rc = cl_eeprom_read_mac_addr(enetaddr, CONFIG_SYS_I2C_EEPROM_BUS);
+	rc = cl_eeprom_read_mac_addr(enetaddr, eeprom_bus);
 	if (rc)
 		return rc;
 
 	if (!is_valid_ether_addr(enetaddr))
 		return -1;
 
-	return eth_setenv_enetaddr("ethaddr", enetaddr);
+	return eth_setenv_enetaddr(env_var, enetaddr);
 }
 
+#define CONFIG_SYS_I2C_EEPROM_BASE_BUS 0
 int board_eth_init(bd_t *bis)
 {
-	int res;
+	if (handle_mac_address("ethaddr", CONFIG_SYS_I2C_EEPROM_BUS))
+		puts("No MAC address found for primary NIC\n");
 
-	res = handle_mac_address();
-	if (res)
-		puts("No MAC address found\n");
+	if (handle_mac_address("eth1addr", CONFIG_SYS_I2C_EEPROM_BASE_BUS))
+		puts("No MAC address found for secondary NIC\n");
 
 	cm_fx6_set_enet_iomux();
 	/* phy reset */
@@ -456,6 +457,11 @@ void ft_board_setup(void *blob, bd_t *bd)
 		fdt_find_and_setprop(blob,
 				     "/soc/aips-bus@02100000/ethernet@02188000",
 				     "local-mac-address", enetaddr, 6, 1);
+	}
+
+	if (eth_getenv_enetaddr("eth1addr", enetaddr)) {
+		fdt_find_and_setprop(blob, "/eth@pcie", "local-mac-address",
+				     enetaddr, 6, 1);
 	}
 }
 #endif
