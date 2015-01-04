6e4013d8ff36f81e6284f4e9c7b67e76ef04275d
Nikita Kiryanov <nikita@compulab.co.il>
2015-01-04 10:32:05 +0200

    spl: nand: remove code duplication
    Remove code duplication in spl_nand_load_image().
    
    Signed-off-by: Nikita Kiryanov <nikita@compulab.co.il>

--- common/spl/spl_nand.c
+++ common/spl/spl_nand.c
@@ -10,6 +10,19 @@
 #include <asm/io.h>
 #include <nand.h>
 
+static int spl_nand_load_element(int offset, struct image_header *header)
+{
+	int err;
+
+	err = nand_spl_load_image(offset, sizeof(*header), (void *)header);
+	if (err)
+		return err;
+
+	spl_parse_image_header(header);
+	return nand_spl_load_image(offset, spl_image.size,
+				   (void *)spl_image.load_addr);
+}
+
 void spl_nand_load_image(void)
 {
 	struct image_header *header;
@@ -61,24 +74,12 @@ void spl_nand_load_image(void)
 	}
 #endif
 #ifdef CONFIG_NAND_ENV_DST
-	nand_spl_load_image(CONFIG_ENV_OFFSET,
-		sizeof(*header), (void *)header);
-	spl_parse_image_header(header);
-	nand_spl_load_image(CONFIG_ENV_OFFSET, spl_image.size,
-		(void *)spl_image.load_addr);
+	spl_nand_load_element(CONFIG_ENV_OFFSET, header);
 #ifdef CONFIG_ENV_OFFSET_REDUND
-	nand_spl_load_image(CONFIG_ENV_OFFSET_REDUND,
-		sizeof(*header), (void *)header);
-	spl_parse_image_header(header);
-	nand_spl_load_image(CONFIG_ENV_OFFSET_REDUND, spl_image.size,
-		(void *)spl_image.load_addr);
+	spl_nand_load_element(CONFIG_ENV_OFFSET_REDUND, header);
 #endif
 #endif
 	/* Load u-boot */
-	nand_spl_load_image(CONFIG_SYS_NAND_U_BOOT_OFFS,
-		sizeof(*header), (void *)header);
-	spl_parse_image_header(header);
-	nand_spl_load_image(CONFIG_SYS_NAND_U_BOOT_OFFS,
-		spl_image.size, (void *)spl_image.load_addr);
+	spl_nand_load_element(CONFIG_SYS_NAND_U_BOOT_OFFS, header);
 	nand_deselect();
 }
